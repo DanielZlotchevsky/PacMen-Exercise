ed8ba3968d395ff3a922b3c6b6a3260818a81dd9
DanielZlotchevsky <danjerz77@gmail.com>
2022-03-27 00:13:05 -0400

    Initial creation: lags out browser to no response

--- PacMen.js
+++ PacMen.js
@@ -1,11 +1,32 @@
 var pos = 0;
-    
+var direction = 0;
+
 const pacArray1 = ['PacMan1.png', 'PacMan2.png'];
 const pacArray2 = ['PacMan3.png', 'PacMan4.png'];
-var direction = 0;
+const fruitArray = ['Apple.png', 'Orange.png', 'Cherry.png', 'StrawBerry.png']
+
 const pacMen = []; // This array holds all the pacmen
+const fruit = [];
 
+let score = 0;
+let timer = 0;
 
+let container = document.querySelector('.container');
+
+var clientWidth = document.getElementById('container').clientWidth;
+var clientHeight = document.getElementById('container').clientHeight;
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
+
+let marginLeft = (winWidth - clientWidth) / 2;
+let marginRight = winWidth - marginLeft;
+let marginTop = (winHeight - clientHeight) / 2;
+let marginBottom = winHeight - marginTop;
+
+let scoreLabel = document.getElementById('Score');
+scoreLabel.innerText = `Score: ${score}`;
+
+/*-----------------------------------------------------Random Number Generation----------------------------------------------------------------*/
 //Gives a random value for valocity that is either negative or positive within the scale
 function setToRandomV(scale) {
     return {
@@ -14,27 +35,49 @@ function setToRandomV(scale) {
     }
 }
 
-//Gives a random X and Y value for the initial position
-function setToRandomP(scale) {
+
+// Creates a random X & Y postition within the container
+function setToRandomPos(minX, maxX, minY, maxY) {
+    let maxx = maxX - 100;
+    let maxy = maxY - 100;
     return {
-        x: Math.random() * scale,
-        y: Math.random() * scale
+        x: Math.floor(Math.random() * (maxx - minX + 1) + minX),
+        y: Math.floor(Math.random() * (maxy - minY + 1) + minY)
     }
 }
 
+/*-----------------------------------------------------Image PNG control / Animation----------------------------------------------------------------*/
 
 //Cycles through the image arrays to animate pacmans mouth movements
 function pacCycle(array, item) {
     
     if (item.cycleTimer % 2 == 0) {
-        
         item.cycleTimer += 1;
         return array[0];
     }
     if (item.cycleTimer % 2 != 0) {
-        
         item.cycleTimer += 1;
         return array[1];
+    }
+}
+
+function fruitCycle(array, item) {
+    
+    if (item.fruitClock == 0) {
+        item.fruitClock += 1;
+        return array[0];
+    }
+    if (item.fruitClock = 1) {
+        item.fruitClock += 1;
+        return array[1];
+    }
+    if (item.fruitClock == 2) {
+        item.fruitClock += 1;
+        return array[2];
+    }
+    if (item.fruitClock = 3) {
+        item.fruitClock = 0;
+        return array[3];
     }
 }
 
@@ -47,10 +90,17 @@ function pacAngle(item) {
     return (degrees + 'deg') 
 }
 
+
+
+/*-----------------------------------------------------PacMan Generation / Creation----------------------------------------------------------------*/
+
 // Factory to make a PacMan at a random position with random velocity
 function makePac() {
     let velocity = setToRandomV(20);
-    let position = setToRandomP(window.innerHeight - 100);
+    let position = setToRandomPos(marginLeft, marginRight, marginTop, marginBottom);
+    console.log(`width start: ${marginLeft} Width end: ${marginRight} X Pos: ${position.x}`)
+    
+    console.log(`Height start: ${marginTop} Height end: ${marginBottom} X Pos: ${position.y}`)
     var cycleTimer = 0; //used to determin pacCycle function
     // Add image to div id = game
     let game = document.getElementById('game');
@@ -61,6 +111,7 @@ function makePac() {
     newimg.width = 100;
     newimg.style.left = position.x;
     newimg.style.top = position.y;
+    console.log(newimg.style.left)
     game.appendChild(newimg);
     
     
@@ -73,17 +124,13 @@ function makePac() {
     }
 }
 
-
-/* Removes the 'Start Game' button to stop the infinite scaling 
-velocity with multiple clicks, then runs the update() function. */
-function removeButtonAndStart(){
-    if (pacMen.length == 0) { window.alert('Please add a PacMan first!')         
-    } else {
-    var startButton = document.getElementById('startButton');
-    startButton.parentNode.removeChild(startButton);
-    update();
-    }
+//Create a PacMan and add it to the pacMen array
+function makeOne() {
+    pacMen.push(makePac()); // add a new PacMan
 }
+
+/*-----------------------------------------------------PacMan Movement / Collision control----------------------------------------------------------------*/
+
 
 //loop over pacmen array and move each one and move image in DOM
 function update() {
@@ -101,27 +148,126 @@ function update() {
     })
     
     setTimeout(update, 100);
+    
 }
 
 
 /*changes direction of movement upon hitting a wall, as well as then 
 matching image and image rotation to fit the new velocity vector */
 function checkCollisions(item) {
-    if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
-    item.position.x + item.velocity.x < 0) {
+    if (item.position.x + item.velocity.x + item.newimg.width > marginRight ||
+    item.position.x + item.velocity.x < marginLeft) {
         item.velocity.x = -item.velocity.x;
         item.newimg.style.transform = `rotate(0deg)`;
         item.newimg.style.transform = `rotate(${pacAngle(item)})`;
     }
-    if(item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
-    item.position.y + item.velocity.y < 0) {
+    if(item.position.y + item.velocity.y + item.newimg.height > marginBottom ||
+    item.position.y + item.velocity.y < marginTop) {
         item.velocity.y = -item.velocity.y;
         item.newimg.style.transform = `rotate(0deg)`;
         item.newimg.style.transform = `rotate(${pacAngle(item)})`;
     }
 }
 
-//Create a PacMan and add it to the pacMen array
-function makeOne() {
-    pacMen.push(makePac()); // add a new PacMan
+
+/*------------------------------------------------------------HTML DOM control------------------------------------------------------------------------*/
+
+
+/* Removes the 'Start Game' button to stop the infinite scaling 
+velocity with multiple clicks, then runs the update() function. */
+function removeButtonAndStart(){
+    var startButton = document.getElementById('startButton');
+    startButton.parentNode.removeChild(startButton);
+    pacmanMakeTimer();
+    scoreKeep();
+    makeOneFruit();
+    updateFruit();
+    updateFruitImg();
+    update();
 }
+
+
+/*------------------------------------------------------------Cursor Follow------------------------------------------------------------------------*/
+
+function makeOneFruit() {
+    fruit.push(makeFruit()); // add a new Fruit
+}
+
+function makeFruit () {
+    let game = document.getElementById('game');
+    let newFruit = document.createElement('img');
+    newFruit.style.position = 'absolute';
+    newFruit.src = 'Apple.png';
+    newFruit.width = 100;
+    var fruitClock = 0;
+    newFruit.style.left = '900px';
+    newFruit.style.top = '250px';
+    game.appendChild(newFruit);
+
+
+    return {
+        position,
+        velocity,
+        newFruit,
+        fruitClock
+    }
+}
+
+function updateFruitImg() {
+    fruit.forEach((item) => {      
+        item.newimg.src = fruitCycle(fruitArray, item);    
+    })
+    
+    setTimeout(updateFruitImg, 500);
+}
+
+function updateFruit() {
+    fruit.forEach((item) => {
+        checkCollisionsFruit(item) //must change           
+    })
+    
+    setTimeout(updateFruit, 100);
+}
+
+function checkCollisionsFruit(item) {
+    newFunction();
+
+        function newFunction() {
+            pacMen.forEach((pac) => {
+                if (Number(item.style.left) < Number(pac.position.x + pac.velocity.x + pac.newimg.width) < Number(item.style.left) + 100 ||
+                    Number(item.style.top) < Number(pac.position.y + pac.velocity.y + pac.newimg.height) < Number(item.style.top) + 100) {
+                    clearTimeout(update);
+                    clearTimeout(scoreKeep);
+                    window.alert(`CONGRATS!! Your score is: ${score}`);
+                }
+            });
+        }
+}
+
+function scoreKeep() {
+    let multiplier = 1 + (0.1 * pacMen.length);
+    score = (score + timer * multiplier);
+    timer += 1;
+    setTimeout(scoreKeep, 1000)
+    return score;
+}
+
+function pacmanMakeTimer() {
+    makeOne()
+    setTimeout(makeOne, 2000)
+}
+
+document.body.addEventListener("mousemove", (event) => {
+
+    /* ClientX and ClientY return the position of clients cursor from top left of the screen*/
+    var x = event.clientX;
+    var y = event.clientY;
+
+    while ( marginLeft < x < marginRight && marginTop < y < marginBottom ) {
+        if (fruit.length > 0) {
+        fruit[0].style.left = `${x - 50}px`;
+        fruit[0].style.top = `${y - 50}px`;
+        }
+    }
+    
+});
